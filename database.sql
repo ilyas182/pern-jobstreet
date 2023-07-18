e69cedc192272048667b887215967437449a9b9c
ilyas182 <ilyas2104@gmail.com>
2023-07-18 11:19:01 +0800

    initial commit

---
+++ database.sql
@@ -0,0 +1,57 @@
+CREATE TABLE users (
+id serial PRIMARY KEY,
+name varchar NOT NULL,
+email varchar NOT NULL UNIQUE,
+password varchar NOT NULL,
+bio text,
+location text,
+applications int,
+ );
+
+CREATE TABLE jobApplications (
+id serial PRIMARY KEY,
+job_id integer NOT NULL REFERENCES jobs (id)
+user_id integer NOT NULL REFERENCES users (id)
+ );
+
+CREATE TABLE savedJobs (
+id serial PRIMARY KEY,
+job_id integer NOT NULL REFERENCES jobs (id)
+user_id integer NOT NULL REFERENCES users (id)
+ );
+
+CREATE TABLE employers (
+id serial PRIMARY KEY,
+contact_name varchar NOT NULL,
+contact varchar NOT NULL,
+email varchar NOT NULL UNIQUE,
+password varchar NOT NULL,
+businessName varchar NOT NULL,
+bio text,
+NoOfJobs int,
+ );
+
+CREATE TABLE jobs (
+id serial PRIMARY KEY,
+title varchar NOT NULL,
+description text NOT NULL,
+pay varchar,
+time timestamp,
+industry varchar,
+location varchar,
+closingDate date
+employer_id integer NOT NULL REFERENCES employers (id)
+ );
+
+CREATE TABLE jobQn (
+id serial PRIMARY KEY,
+questions varchar NOT NULL,
+job_id integer NOT NULL REFERENCES jobs (id)
+ );
+
+CREATE TABLE jobAns (
+id serial PRIMARY KEY,
+answers varchar NOT NULL,
+jobQn_id integer NOT NULL REFERENCES jobQn (id)
+ );
+
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors')
+const pool = require('./config/db')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Register the tweet routes
+// app.use('/tweets', tweetsRoutes);
+//create tweet
+// app.use("/api/home", tweetsRoutes)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
