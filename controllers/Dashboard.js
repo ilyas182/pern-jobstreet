23660ef7b8efde6532d44a88e7ef19c2d6a520e6
ilyas182 <ilyas2104@gmail.com>
2023-07-20 14:54:11 +0800

    return user name only when authorized

--- controllers/Dashboard.js
+++ controllers/Dashboard.js
@@ -2,7 +2,11 @@ const pool = require('../config/db')
 
 async function authorize(req, res){
     try {
-        res.json(req.user);
+        // req.user is holding the payload
+        // res.json(req.user);
+
+        const user = await pool.query("SELECT name FROM users WHERE id=$1", [req.user]);
+        res.json(user.rows[0])
     } catch (error) {
         console.error(error.message);
         res.status(403).json("Not authorized");
