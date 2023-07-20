ffecc9c89428d0ec93d6982f75d020908a410f37
ilyas182 <ilyas2104@gmail.com>
2023-07-20 10:10:42 +0800

    bcrypt new user password

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,16 +1,25 @@
 const pool = require('../config/db')
+const bcrypt = require('bcrypt');
 
 async function create(req,res) {
     try {
         // Step 1: destructure req.body 
         const { name, email, password } = req.body;
+
         // Step 2: check if user exist (if user exist then throw error)
         const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
-        res.json(user.rows)
+        if (user.rows.length !== 0){
+            res.status(401).send("User already exists");
+        }
+
         // Step 3: bcrypt user password
+        const saltRound = parseInt(process.env.SALTROUND);
+        const salt = await bcrypt.genSalt(saltRound);
+        const bcryptPassword = await bcrypt.hash(password, salt);
 
         // Step 4: enter the new user inside our database
-
+        const newUser = await pool.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptPassword])
+        res.json(newUser.rows[0]);
         // Step 5: generate jwt
     } catch (error) {
         console.error(error.message);
