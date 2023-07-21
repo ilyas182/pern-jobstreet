1218d81b0d584e1ed809e904c803407430634c17
ilyas182 <ilyas2104@gmail.com>
2023-07-21 11:47:32 +0800

    add register form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,44 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 export default function Register() {
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: "",
+        name: ""
+    });
+
+    const {email, password, name} = inputs;
+    const onChange = (e) => {
+        setInputs({...inputs, [e.target.name]:e.target.value})
+    };
+
     return (
         <Fragment>
-            Register
+            <h1 className="text-center">Register</h1>
+            <form>
+                <input 
+                    type='email' 
+                    name='email' 
+                    placeholder="email" 
+                    className="form-control my-3"
+                    value = {email}
+                    onChange={e => onChange(e)}/>
+                <input 
+                    type='password' 
+                    name='password' 
+                    placeholder="password" 
+                    className="form-control my-3"
+                    value = {password}
+                    onChange={e => onChange(e)}/>
+                <input 
+                    type='text' 
+                    name='name' 
+                    placeholder="name" 
+                    className="form-control my-3"
+                    value = {name}
+                    onChange={e => onChange(e)}/>
+                <button className="btn btn-success btn-block">Submit</button>
+            </form>
         </Fragment>
     )
 }
