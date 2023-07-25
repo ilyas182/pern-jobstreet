a2c3483358e29cf98b45189214aee52176400117
ilyas182 <ilyas2104@gmail.com>
2023-07-25 20:40:23 +0800

    add bookmark job function

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -46,13 +46,35 @@ export default function JobCard({ job }) {
         // getId();
         checkIfBookmarked();
     }, []);
+
+    async function handleBookmark(){
+        try {
+            userId = await getId();
+            const body = {user_id : userId, job_id: job.id};
+            console.log(body)
+            const response = await fetch("http://localhost:3001/api/main/save", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body)
+            })
+            if (response.ok){
+                const jsonData = await response.json();
+                console.log(jsonData.message);
+                setBookmark(!bookmark);
+            }
+            
+        } catch (error) {
+            console.error(error.message)
+        }
+        
+    }
     return (
     <>
     <h2>{job.title}</h2>
     <p>Salary: {job.pay}</p> 
     {job.level ? (<p>Experience: {job.level}</p>) : (<p>Experience: Not specified</p>)}
     <button onClick={() => navigate(`/job/${job.id}/apply`)}>Apply</button>
-    {!bookmark && <button>Bookmark</button>}
+    {!bookmark && <button onClick={handleBookmark}>Bookmark</button>}
     {bookmark && <button disabled="true">Bookmarked</button>}
     
     <hr/>
