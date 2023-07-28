c41959dbd73056b3253cc95f4044ea4ecc0ef74c
ilyas182 <ilyas2104@gmail.com>
2023-07-28 12:56:30 +0800

    build

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const dashboardRoute = require('./routes/Dashboard');
 const employerRoute = require('./routes/Employer');
 const jobRoute = require('./routes/Jobs');
 const app = express();
-const favicon = require("serve-favicon")
+// const favicon = require("serve-favicon")
 
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+// app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
 //ROUTES
 app.use("/api/main", usersRoute);
